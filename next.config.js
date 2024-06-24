11e70f8660239ec496951d643d4e6133ff4d317b
lndshubin <lndshubin@gmail.com>
2024-06-24 15:49:28 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // Allow all origins
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
   // Turn off React StrictMode for now, as react-aria (used by Plasmic)
   // has some troubles with it. See
   // https://github.com/adobe/react-spectrum/labels/strict%20mode
   reactStrictMode: false,
+
 };
 
 module.exports = nextConfig;
